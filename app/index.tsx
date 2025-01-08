461ce09e35c9e5e102653e5ff98950f726e74dd8
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2025-01-08 09:54:06 +0200

    Navigate to the about route

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -6,6 +7,9 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -18,6 +22,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
