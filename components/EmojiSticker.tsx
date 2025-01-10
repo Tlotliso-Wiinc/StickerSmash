a0817c2a0f16c7fba28904b3fc3baf49fcabe0fb
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2025-01-10 14:19:57 +0200

    Add a tap gesture

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native';
-import Animated from 'react-native-reanimated';
 import { type ImageSource } from 'expo-image';
+
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 type Props = {
   imageSize: number;
@@ -8,6 +10,8 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+  
   return (
     <View style={{ top: -350 }}>
       <Animated.Image
